20750a1058f589966dd0977c6a10e83ec46561a3
unknown <you@example.com>
2018-03-28 16:27:46 +0800

    PraticeATest in section2 is successed!

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,23 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String,Integer> result = new HashMap<>();
+        for (int i = 0; i < collection1.size(); i++) {
+            String str = collection1.get(i);
+            if (result.containsKey(str)) {
+                int count = result.get(str);
+                count++;
+                result.put(str, count);
+            } else {
+                result.put(str,1);
+            }
+        }
+        return result;
     }
 }
