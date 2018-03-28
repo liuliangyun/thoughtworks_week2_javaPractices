a787db4514051ce44863bdd268ece3318f4adcba
unknown <you@example.com>
2018-03-28 17:29:10 +0800

    PracticeATest in section3 is successed!

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,7 +6,13 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        List<String> list2 = object.get("value");
+        for (String s : collectionA.keySet()) {
+            if (list2.contains(s)) {
+                int count = collectionA.get(s) - 1;
+                collectionA.put(s, count);
+            }
+        }
+        return collectionA;
     }
 }
